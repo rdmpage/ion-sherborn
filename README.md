726ee22374f48e61e0ccc3ac69fc423d9eca1ce9
Roderic Page <r.page@bio.gla.ac.uk>
2017-05-16 13:26:35 +0100

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# ion-sherborn
-Mapping between ION names and entries in Index Animaliam
+# ION to Sherborn’s Index Animalium 
+
+Mapping between ION names and entries in Index Animalium and BHL. For background on this project see paper and slides below.
+
+## References
+
+Richard, J. M., Pilsk, S. C., & Kalfatovic, M. R. (2016, January 7). Unlocking Index Animalium: From paper slips to bytes and bits. ZooKeys. Pensoft Publishers. [https://doi.org/10.3897/zookeys.550.9673](https://doi.org/10.3897/zookeys.550.9673)
+
+Sherborn’s Index Animalium integration into ION: access to all [https://www.slideshare.net/iczn/4-sherborns-index-animalium-integration-into-ion](https://www.slideshare.net/iczn/4-sherborns-index-animalium-integration-into-ion)
+
---
+++ parse.php
@@ -0,0 +1,51 @@
+<?php
+
+// Harvest Sherborn mapping from ION
+
+require_once(dirname(__FILE__) . '/simplehtmldom_1_5/simple_html_dom.php');
+
+$basedir = 'html';
+$files1 = scandir(dirname(__FILE__) . '/' . $basedir);
+
+foreach ($files1 as $directory)
+{
+	if (preg_match('/^\d+$/', $directory))
+	{	
+		$files2 = scandir(dirname(__FILE__) . '/' . $basedir . '/' . $directory);
+		foreach ($files2 as $filename)
+		{
+			if (preg_match('/^(?<id>\d+)\.html$/', $filename, $m))
+			{	
+				$html = file_get_contents(dirname(__FILE__) . '/' . $basedir . '/' . $directory . '/' . $filename);
+
+				$dom = str_get_html($html);
+
+				$lis = $dom->find('ul li');
+				foreach ($lis as $li)
+				{
+					if (preg_match('/(?<micro>.*)\s+\[Index Animalium Entry\]/', $li->plaintext, $m)) 
+					{ 
+						$obj = new stdclass;
+						$obj->citation = $m['micro'];
+		
+						foreach ($li->find('a') as $a)
+						{
+							if (preg_match('/biodiversitylibrary/', $a->href))
+							{
+								$obj->PageID = str_replace('http://biodiversitylibrary.org/page/', '', $a->href);
+							}		
+							else
+							{
+								$obj->IndexAnimalium = $a->href;
+							}
+						}
+		
+						print_r($obj);
+					}
+				}
+			}
+		}
+	}
+}
+
+?>
